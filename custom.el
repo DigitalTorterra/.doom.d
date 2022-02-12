e453696bdb9f9cb1639d80ad406929e61365e990
Patrick Faley <patrick.faley@gmail.com>
2022-02-12 09:54:01 -0600

    Initial commit

---
+++ custom.el
@@ -0,0 +1,57 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#fbf1c7" "#9d0006" "#79740e" "#b57614" "#076678" "#b16286" "#427b58" "#282828"])
+ '(custom-safe-themes
+   '("6e14157d0c8857e81035e6c7131dc17e4115b3911c82a1fd32e528aec8e89eab" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "1f50a7274cd56f28713e1694600ec7b8f2fd1c7d2ef38c5e7378a26931605409" "cebda623e3f9ecd4a4af08d92c406bc4517e323d9259d5156f43901860224e30" "7fb8ead14c61c272f913baa0678ae71239c5fdb9ab7b6d32109ab3c63c6a6df3" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "63bfcabeb44559c67d8827dc68cd6c4a6d3ce35ef4504343af12d42f24894e00" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "3df5335c36b40e417fec0392532c1b82b79114a05d5ade62cfe3de63a59bc5c6" "74ba9ed7161a26bfe04580279b8cad163c00b802f54c574bfa5d924b99daa4b9" "c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" default))
+ '(exwm-floating-border-color "#28323e")
+ '(fci-rule-color "#504945")
+ '(highlight-tail-colors
+   ((("#354f57" "#081611" "green")
+     . 0)
+    (("#394f5e" "#0d1617" "brightcyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#a89984"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#79740e"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#928374"))
+ '(objed-cursor-color "#9d0006")
+ '(package-selected-packages
+   '(npm-mode haskell-mode svelte-mode lsp-mode rustic latex-preview-pane auctex elpy cider paredit clojure-mode))
+ '(pdf-view-midnight-colors (cons "#282828" "#fbf1c7"))
+ '(rustic-ansi-faces
+   ["#fbf1c7" "#9d0006" "#79740e" "#b57614" "#076678" "#b16286" "#427b58" "#282828"])
+ '(vc-annotate-background "#fbf1c7")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#79740e")
+    (cons 40 "#8d7410")
+    (cons 60 "#a17512")
+    (cons 80 "#b57614")
+    (cons 100 "#b3620e")
+    (cons 120 "#b14e08")
+    (cons 140 "#af3a03")
+    (cons 160 "#af472e")
+    (cons 180 "#b0545a")
+    (cons 200 "#b16286")
+    (cons 220 "#aa415b")
+    (cons 240 "#a32030")
+    (cons 260 "#9d0006")
+    (cons 280 "#9a2021")
+    (cons 300 "#97413c")
+    (cons 320 "#946258")
+    (cons 340 "#504945")
+    (cons 360 "#504945")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
+
+
